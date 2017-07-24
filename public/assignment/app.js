d0cc020afbe660b83fd386b713f15a46aae5538f
Abhilash360 <abhilash.kadupatil@gmail.com>
2017-07-23 20:25:16 -0400

    [CS5610-03] Fixed Urls

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,6 +3,8 @@ var app = angular.module("WebAppMaker", ["ngRoute"]);
 app.controller("loginController", loginController);
 
 function loginController($scope) {
-    $scope.uid = 5;
+    $scope.uid = 1;
+    $scope.wid = 2;
+    $scope.pid = 3;
     //alert("Hello")
 }
