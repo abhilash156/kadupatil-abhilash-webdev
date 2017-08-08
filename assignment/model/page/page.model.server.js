5890a774c5573ca469c096c3d3c981caac2a1520
Abhilash360 <abhilash.kadupatil@gmail.com>
2017-08-08 03:07:16 -0400

    [CS5610-05] Fixed Page Server Service

---
+++ assignment/model/page/page.model.server.js
@@ -0,0 +1,60 @@
+var mongoose = require("mongoose");
+var pageSchema = require("./page.schema.server");
+mongoose.Promise = require("q").Promise;
+
+var pageModel = mongoose.model("PageModel", pageSchema);
+
+require("../models.server");
+
+pageModel.createPage = createPage;
+pageModel.findAllPagesForWebsite = findAllPagesForWebsite;
+pageModel.findPageById = findPageById;
+pageModel.updatePage = updatePage;
+pageModel.deletePage = deletePage;
+
+module.exports = pageModel;
+
+function createPage(websiteId, page) {
+    page._website = websiteId;
+    return pageModel.create(page);
+}
+
+function findAllPagesForWebsite(websiteId) {
+    return pageModel.find({"_website": websiteId});
+}
+
+function findPageById(pageId) {
+
+    return pageModel.findById(pageId);
+}
+
+function updatePage(pageId, page) {
+
+    delete page._website;
+    delete page.dateCreated;
+    return pageModel.update({_id: pageId}, {$set: page});
+}
+
+function deletePage(pageId) {
+
+    return pageModel.remove({_id: pageId});
+}
+
+
+var pages = [
+    {"name": "Post 1", "_website": "59895e2413afb7153c0c0259", "description": "Lorem"},
+    {"name": "Post 2", "_website": "59895e2413afb7153c0c0259", "description": "Lorem"},
+    {"name": "Post 3", "_website": "59895e2413afb7153c0c0259", "description": "Lorem"},
+    {"name": "Horizon Zero Dawn", "_website": "59895e2413afb7153c0c025e", "description": "Horizon Zero Dawn is an " +
+    "action role-playing video game developed by Guerrilla Games and published by Sony Interactive " +
+    "Entertainment for PlayStation 4 and released in early 2017. The plot revolves around Aloy, a hunter " +
+    "and archer living in a world overrun by robots. Having been an outcast her whole life, she sets out to " +
+    "discover the dangers that kept her sheltered. The character uses ranged weapons and a spear and stealth " +
+    "tactics to combat the mechanised creatures, whose remains can be looted for resources."
+    }
+];
+
+createPage(pages[0]._website, pages[0]);
+createPage(pages[1]._website, pages[1]);
+createPage(pages[2]._website, pages[2]);
+createPage(pages[3]._website, pages[3]);
--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -1,18 +1,6 @@
 var app = require("../../express");
 
-var pages = [
-    {"_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem"},
-    {"_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem"},
-    {"_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem"},
-    {
-        "_id": "333", "name": "Horizon Zero Dawn", "websiteId": "222", "description": "Horizon Zero Dawn is an " +
-    "action role-playing video game developed by Guerrilla Games and published by Sony Interactive " +
-    "Entertainment for PlayStation 4 and released in early 2017. The plot revolves around Aloy, a hunter " +
-    "and archer living in a world overrun by robots. Having been an outcast her whole life, she sets out to " +
-    "discover the dangers that kept her sheltered. The character uses ranged weapons and a spear and stealth " +
-    "tactics to combat the mechanised creatures, whose remains can be looted for resources."
-    }
-];
+var pageModel = require("../model/page/page.model.server");
 
 app.post("/api/website/:websiteId/page", createPage);
 app.get("/api/website/:websiteId/page", findAllPagesForWebsite);
@@ -24,61 +12,55 @@ function createPage(request, response) {
     var websiteId = request.params.websiteId;
     var page = request.body;
 
-    page._id = (new Date()).getTime() + "";
-    page.websiteId = websiteId;
-    pages.push(page);
-    response.json(page);
+    pageModel.createPage(websiteId, page)
+        .then(function (newPage) {
+            response.send(newPage);
+        }, function (error) {
+            response.sendStatus(404).error(error);
+        });
 }
 
 function findAllPagesForWebsite(request, response) {
     var websiteId = request.params.websiteId;
 
-    var sites = [];
-    for (var p in pages) {
-        if (pages[p].websiteId === websiteId) {
-            sites.push(pages[p]);
-        }
-    }
-    return response.json(sites);
+    pageModel.findAllPagesForWebsite(websiteId)
+        .then(function (pages) {
+            response.send(pages);
+        }, function (error) {
+            response.sendStatus(404).error(error);
+        });
 }
 
 function findPageById(request, response) {
     var pageId = request.params.pageId;
 
-    for (var p in pages) {
-        var _page = pages[p];
-        if (_page._id === pageId) {
-            response.json(_page);
-            return;
-        }
-    }
-    response.sendStatus(404);
+    pageModel.findPageById(pageId)
+        .then(function (page) {
+            response.send(page);
+        }, function (error) {
+            response.sendStatus(404).error(error);
+        });
 }
 
 function updatePage(request, response) {
     var page = request.body;
     var pageId = request.params.pageId;
 
-    for (var p in pages) {
-        if (pages[p]._id === pageId) {
-            page._id = pageId;
-            pages[p] = page;
-            response.json(page);
-            return;
-        }
-    }
-    response.sendStatus(404);
+    pageModel.updatePage(pageId, page)
+        .then(function () {
+            response.sendStatus(200);
+        }, function (error) {
+            response.sendStatus(404).error(error);
+        });
 }
 
 function deletePage(request, response) {
     var pageId = request.params.pageId;
 
-    for (var p in pages) {
-        if (pages[p]._id === pageId) {
-            pages.splice(p, 1);
+    pageModel.deletePage(pageId)
+        .then(function () {
             response.sendStatus(200);
-            return;
-        }
-    }
-    response.sendStatus(404);
+        }, function (error) {
+            response.sendStatus(404).error(error);
+        });
 }
